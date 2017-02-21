42d878ad358c9d9f12823450e36c97782d7d068c
Simen Ullern <simenullern@gmail.com>
2017-02-21 14:09:29 +0100

    Fjernet in-line HTML script og lagt til jquery effekter
    Script.js inneholder nå kode som før delvis fantes i html-koden. Jquery
    syntax brukt fremfor CSS typ display:none

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 
-function send() {
-    document.write('Skjemaet ble sendt');
+function setup_graphic() {
+     $( function() {
+            // setup graphic EQ
+            $( "#eq2 > span" ).each(function() {
+                // read initial values from markup and remove that
+                var value = parseInt( $( this ).text(), 10 );
+                $( this ).empty().slider({
+                    value: value,
+                    range: "min",
+                    animate: true,
+                    orientation: "vertical"
+                });
+            });
+        } );
+        $( function() {
+            $( "#slider" ).slider();
+        } );
+    
+    $( function() {
+      $("#eq2").hide();
+      } );
 };
 
 
-$(document).ready(function() {
-    $('.radios').click(function(event) {
+$(document).ready(function() {;
+    $('#fram').click(function(event) {
       event.preventDefault();
-      $(this).hide("slower");
+      $('#eq').hide("fast");
+        $('#eq2').show("fast");
+        // null som parameter for å fjerne animasjon
+    });
+                              
+    $('#tilbake').click(function(event) {
+      event.preventDefault();
+      $('#eq2').hide("fast");
+        $('#eq').show("fast");
     });
 });
+
