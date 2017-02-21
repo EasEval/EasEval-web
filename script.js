1cf0422000346a62be65bfe8f68dc91081bb4fb1
Simen Ullern <simenullern@gmail.com>
2017-02-21 15:59:13 +0100

    Lese verdier fra sliders + småtterier
    Hvis man trykker contact leses verdi fra sliders. Hver slider har trengt
    en egen id.

--- script.js
+++ script.js
@@ -26,9 +26,11 @@ function setup_graphic() {
 $(document).ready(function() {;
     $('#fram').click(function(event) {
       event.preventDefault();
+        
       $('#eq').hide("fast");
         $('#eq2').show("fast");
         // null som parameter for å fjerne animasjon
+        
     });
                               
     $('#tilbake').click(function(event) {
@@ -36,5 +38,29 @@ $(document).ready(function() {;
       $('#eq2').hide("fast");
         $('#eq').show("fast");
     });
+                              
+    $('#contact').click(function(event) {
+        var q1 = $('#slider').slider("option", "value");
+        var s1 = $('#s1').slider("option", "value");
+        var s2 = $('#s2').slider("option", "value");
+        var s3 = $('#s3').slider("option", "value");
+        var s4 = $('#s4').slider("option", "value");
+        var s5 = $('#s5').slider("option", "value");
+            
+        document.write(q1 +" " + s1 + " " + s2
+                       + " " +s3 + " "+ s4 + " " + s5);
+    });
 });
+
+
+/* Lese verdier, tanke:
+    if ($('eq2')is(":visible")) {
+        var q1 = $('#slider').slider("option", "value");
+        var s1 = $('#s1').slider("option", "value");
+        var s2 = $('#s2').slider("option", "value");
+        var s3 = $('#s3').slider("option", "value");
+        var s4 = $('#s4').slider("option", "value");
+        var s5 = $('#s5').slider("option", "value");
+            
+        document.write(q1, s1, s2, s3, s4, s5);*/
 
