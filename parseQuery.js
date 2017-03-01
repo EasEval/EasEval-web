ba15fd03f952dfbbd3285b56e30466ba3e54ba1c
eivindrebnord <eivindrebnord@gmail.com>
2017-03-01 11:45:21 +0100

    Fjernet funksjoner
    Korrigerte submitRecord() med riktige argument

--- parseQuery.js
+++ parseQuery.js
@@ -1,34 +1,14 @@
-/**
- * Created by earebnord on 15.02.17.
- */
 var Parse = require('parse/node').Parse;
 Parse.initialize("asddfkjhg4ey123478wss234q2388dhakjhdgrudb"); Parse.serverURL = 'https://easeval.herokuApp.com/parse';
-var strings;
-var i;
-window.init = function(){
-    strings = ["I am a text element"];
-    i = 1;
-};
-window.testFunksjon = function() {
-    if (i == 1){
-        var nameValue2 = document.getElementById("uniqueID").value;
-        strings.push(nameValue2);
-    }
 
-    console.log(strings[i % 2], i);
-    document.getElementById("textelement").innerHTML = strings[i % 2];
-    document.getElementById("minCounter").innerHTML="Du har klikket ".concat(i, " ganger");
-    i++;
-};
-window.submitRecord = function (username, subjectID, name, record) {
+window.submitRecord = function (record) {
     var exercise = Parse.Object.extend("Exercises");
 
-    //Registrerer ny record med username, subjectID, name (exercise#), record
+    //Registrerer ny record med subjectID, name (exercise#), record
 
     var evaluation = new exercise();
-    evaluation.set("username", username);
-    evaluation.set("subjectID", subjectID);
-    evaluation.set("NAME", name);
+    evaluation.set("subjectID", "TMA4100");
+    evaluation.set("NAME", "testNavn01");
     evaluation.set("rating", record[0]);
     evaluation.set("time", record[1]);
     evaluation.set("lectureAmount", record[2][0]);
