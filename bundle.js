4f971332faad9041ec99660412285c50e3f559eb
eivindrebnord <eivindrebnord@gmail.com>
2017-03-01 13:59:44 +0100

    submitRecord() oppdatert
    Legger nå til et Subjects-objekt som pointer i Exercises-tabellen.

--- bundle.js
+++ bundle.js
@@ -3,33 +3,35 @@ var Parse = require('parse/node').Parse;
 Parse.initialize("asddfkjhg4ey123478wss234q2388dhakjhdgrudb"); Parse.serverURL = 'https://easeval.herokuApp.com/parse';
 
 window.submitRecord = function (record) {
+    //Oppretter instanser av tabeller der data skal legges inn ellers spørres etter
     var exercise = Parse.Object.extend("Exercises");
+    var fag = Parse.Object.extend("Subjects");
 
-    //Registrerer ny record med subjectID, name (exercise#), record
-
-    var evaluation = new exercise();
-    evaluation.set("SUBJECTID", "TMA4100");
-    evaluation.set("NAME", "testNavn01");
-    evaluation.set("rating", record[0]);
-    evaluation.set("time", record[1]);
-    evaluation.set("lectureAmount", record[2][0]);
-    evaluation.set("curriculumAmount", record[2][1]);
-    evaluation.set("googleAmount", record[2][2]);
-    evaluation.set("solutionsAmount", record[2][3]);
-    evaluation.save();
-
-    // Spør etter alle keys kalt item og sletter disse
-
-/*    var query = new Parse.Query(Fag);
-    query.equalTo("ID", "Testfag");
+    var query = new Parse.Query(fag);
+    query.equalTo("ID", "TMA4100");
     query.find({
         success: function(users) {
             for (var i = 0; i < users.length; ++i) {
-                console.log(users[i].get('ID') + ", indeks: " + i + ". Sletter denne!");
-                users[i].destroy();
+
+                //Lagrer Subjects-pointeren
+                var subjectPointer = users[i];
             }
+            var evaluation = new exercise();
+            evaluation.set("SUBJECTID", "TMA4100");
+            evaluation.set("SUBJECT", subjectPointer); //Sender videre Subjects-objektet som pointer
+            evaluation.set("NAME", "testNavn01");
+            evaluation.set("rating", record[0]);
+            evaluation.set("time", record[1]);
+            evaluation.set("lectureAmount", record[2][0]);
+            evaluation.set("curriculumAmount", record[2][1]);
+            evaluation.set("googleAmount", record[2][2]);
+            evaluation.set("solutionsAmount", record[2][3]);
+            evaluation.save();
         }
-    });*/
+    });
+
+    //Prøver Parse.Promise for å ordne asynkrone kall
+
 };
 },{"parse/node":182}],2:[function(require,module,exports){
 module.exports = { "default": require("core-js/library/fn/get-iterator"), __esModule: true };
