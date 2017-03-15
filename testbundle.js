4a54382f7c4f40e8446c00cca9391b97ae0922c2
eivindrebnord <eivindrebnord@gmail.com>
2017-03-15 16:22:49 +0100

    QUnit test suite for back end
    asynkrone DB-tester lagt til (addRecordTest + deleteRecordTest)

--- testbundle.js
+++ testbundle.js
@@ -2,61 +2,62 @@
 var Parse = require('parse/node').Parse;
 Parse.initialize("asddfkjhg4ey123478wss234q2388dhakjhdgrudb"); Parse.serverURL = 'https://easeval.herokuApp.com/parse';
 
-window.submitRecord = function (record) {
-    //Oppretter instanser av tabeller der data skal legges inn ellers spørres etter
-    var exercise = Parse.Object.extend("Exercises");
-    var fag = Parse.Object.extend("Subjects");
+QUnit.test( "deleteRecordTest", function( assert ) {
+    var oName = "Øving YO";
+    var sID = "TMA4100";
+    var done = assert.async();
+    var ovinger = Parse.Object.extend("Exercises");
+    var secondQuery = new Parse.Query(ovinger);
+    setTimeout(function(){
+        deleteExercise(oName, sID).then(function () {
+            console.log("Sletter øving...");
+            secondQuery.equalTo("NAME", oName);
+            //secondQuery.equalTo("SUBJECTID", sID);
+            return secondQuery.find()
 
-    //Prosessering av url når vi kjører fra folk.ntnu
+        }).then(function (users) {
+            console.log("Fant brukere: " + users);
+            assert.ok(users == "", "Removed exercises 'Øving YO'");
+            done();
+        },
+        function (error) {
+            console.log("Fant ingen brukere" + error);
+            done();
+        });
+    }, 1000);
+});
 
-    var len = record[0].length;
-    var query = new Parse.Query(fag);
-    query.equalTo("ID", "TMA4100");
-    return query.find({
-        success: function (pointers) {
-            console.log("Fant objekt-pointer: " + pointers[0]);
-        }
-    }).then(function (pointers) {
-        var subjectPointer = pointers[0];
-        var evaluation = new exercise();
-        evaluation.set("SUBJECTID", record[0].substring(0, len - 2)); //Parser fagnavn fra record[0]
-        evaluation.set("SUBJECT", subjectPointer);  //Sender videre Subjects-objektet som pointer
-        evaluation.set("NAME", "Øving " + record[0].substring(len - 2, len));   //setter øvingsnavnet
-        evaluation.set("rating", record[1]);
-        evaluation.set("time", record[2]);
-        evaluation.set("lectureAmount", record[3][0]);
-        evaluation.set("curriculumAmount", record[3][1]);
-        evaluation.set("googleAmount", record[3][2]);
-        evaluation.set("solutionsAmount", record[3][3]);
-        evaluation.set("comment", record[4]);
-        console.log("Lagret objekt med navn: " + "Øving " + record[0].substring(len - 2, len));
-        return evaluation.save();
-    }).then(function (result) {
-        console.log(".save() sitt callback: " + result);
-        //response.succes("CHECK");
-        return result;
-    }, function (error) {
-        response.error()
-    });
-};
+QUnit.test( "addRecordTest", function( assert ) {
+    var done = assert.async();
+    var ovinger = Parse.Object.extend("Exercises");
+    var firstQuery = new Parse.Query(ovinger);
 
-window.deleteExercise = function (name, subject) {
-    var exercise = Parse.Object.extend("Exercises");
-    var query = new Parse.Query(exercise);
-    query.equalTo("NAME", name);
-    //query.equalTo("SUBJECTID", subject);
-    return query.find({
-
-    }).then( function (users) {
-        console.log("Fant brukere, prøver å slette disse: " + users);
-        return Parse.Object.destroyAll(users);
-    }).then( function(result){
-        console.log("Klarte å slette disse");
-        return result;
-    }, function(error){
-        console.log("Klarte ikke å slette" + error);
-    });
-};
+    setTimeout(function(){
+        firstQuery.count({
+            success: function (callback) {
+                //console.log("Count av alle exercises: " + callback);
+            }
+        }).then(function () {
+            var testRecord = ["TMA4100YO", 10, 10, [10, 10, 10, 10], "hei"];
+            //console.log("Kjører submitRecords()");
+            return submitRecord(testRecord);
+        }).then(function (callback) {
+            var query = new Parse.Query(ovinger);
+            query.equalTo("NAME", "Øving YO");
+            //console.log("Returnert fra submitRecords(): " + callback);
+            return query.find({
+                success: function (exercise) {
+                    assert.ok("Øving YO" == exercise[0].get("NAME"), "Øving YO added!");
+                    done();
+                },
+                error: function (error) {
+                    console.log("Fikk feilmelding: " + error);
+                    done()
+                }
+            });
+        });
+    }, 1000);
+});
 },{"parse/node":182}],2:[function(require,module,exports){
 module.exports = { "default": require("core-js/library/fn/get-iterator"), __esModule: true };
 },{"core-js/library/fn/get-iterator":25}],3:[function(require,module,exports){
