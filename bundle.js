cac0ee6f65a4eadc2f6f04311c1fec56ec9e46fc
eivindrebnord <eivindrebnord@gmail.com>
2017-03-29 09:44:10 +0200

    slider amounts formateres til prosent før de legges i db
    fjerner også prosessering av url

--- bundle.js
+++ bundle.js
@@ -1,27 +1,34 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+//require("blanket.js");
 var Parse = require('parse/node').Parse;
 Parse.initialize("asddfkjhg4ey123478wss234q2388dhakjhdgrudb"); Parse.serverURL = 'https://easeval.herokuApp.com/parse';
 
-window.submitRecord = function (record) {
+window.submitRecord = function (precord) {
     //Oppretter instanser av tabeller der data skal legges inn ellers spørres etter
     var exercise = Parse.Object.extend("Exercises");
     var fag = Parse.Object.extend("Subjects");
+    var record = formatData(precord);
 
-    //Prosessering av url når vi kjører fra folk.ntnu
-
+    /*
+    Prosessering av url når vi kjører fra folk.ntnu
     var len = record[0].length;
+    var fagnavn = record[0].substring(0, len - 2);
+    var ovingsnavn = "Øving " + record[0].substring(len - 2, len);
+    */
+    
     var query = new Parse.Query(fag);
     query.equalTo("ID", "TMA4100");
+
     return query.find({
         success: function (pointers) {
-            console.log("Fant objekt-pointer: " + pointers[0]);
+            //console.log("Fant objekt-pointer: " + pointers[0]);
         }
     }).then(function (pointers) {
         var subjectPointer = pointers[0];
         var evaluation = new exercise();
-        evaluation.set("SUBJECTID", record[0].substring(0, len - 2)); //Parser fagnavn fra record[0]
+        evaluation.set("SUBJECTID", "TMA4100");
         evaluation.set("SUBJECT", subjectPointer);  //Sender videre Subjects-objektet som pointer
-        evaluation.set("NAME", "Øving " + record[0].substring(len - 2, len));   //setter øvingsnavnet
+        evaluation.set("NAME", "Øving 5");
         evaluation.set("rating", record[1]);
         evaluation.set("time", record[2]);
         evaluation.set("lectureAmount", record[3][0]);
@@ -29,10 +36,10 @@ window.submitRecord = function (record) {
         evaluation.set("googleAmount", record[3][2]);
         evaluation.set("solutionsAmount", record[3][3]);
         evaluation.set("comment", record[4]);
-        console.log("Lagret objekt med navn: " + "Øving " + record[0].substring(len - 2, len));
+        //console.log("Lagret objekt med navn: " + "Øving " + record[0].substring(len - 2, len));
         return evaluation.save();
     }).then(function (result) {
-        console.log(".save() sitt callback: " + result);
+        //console.log(".save() sitt callback: " + result);
         //response.succes("CHECK");
         return result;
     }, function (error) {
@@ -57,6 +64,19 @@ window.deleteExercise = function (name, subject) {
         console.log("Klarte ikke å slette" + error);
     });
 };
+
+function formatData(record){
+    var total = 0;
+    for(var i = 0; i < record[3].length; i++){
+        total += record[3][i];
+    }
+    for(var j = 0; j < record[3][j]; j++){
+        record[3][j] = Math.round(record[3][j]*100/total);
+    }
+    console.log(record);
+    console.log(record[3]);
+    return record;
+}
 },{"parse/node":182}],2:[function(require,module,exports){
 module.exports = { "default": require("core-js/library/fn/get-iterator"), __esModule: true };
 },{"core-js/library/fn/get-iterator":25}],3:[function(require,module,exports){
--- parseQuery.js
+++ parseQuery.js
@@ -1,27 +1,33 @@
-require("blanket.js");
+//require("blanket.js");
 var Parse = require('parse/node').Parse;
 Parse.initialize("asddfkjhg4ey123478wss234q2388dhakjhdgrudb"); Parse.serverURL = 'https://easeval.herokuApp.com/parse';
 
-window.submitRecord = function (record) {
+window.submitRecord = function (precord) {
     //Oppretter instanser av tabeller der data skal legges inn ellers spørres etter
     var exercise = Parse.Object.extend("Exercises");
     var fag = Parse.Object.extend("Subjects");
+    var record = formatData(precord);
 
-    //Prosessering av url når vi kjører fra folk.ntnu
-
+    /*
+    Prosessering av url når vi kjører fra folk.ntnu
     var len = record[0].length;
+    var fagnavn = record[0].substring(0, len - 2);
+    var ovingsnavn = "Øving " + record[0].substring(len - 2, len);
+    */
+
     var query = new Parse.Query(fag);
     query.equalTo("ID", "TMA4100");
+
     return query.find({
         success: function (pointers) {
-            console.log("Fant objekt-pointer: " + pointers[0]);
+            //console.log("Fant objekt-pointer: " + pointers[0]);
         }
     }).then(function (pointers) {
         var subjectPointer = pointers[0];
         var evaluation = new exercise();
-        evaluation.set("SUBJECTID", record[0].substring(0, len - 2)); //Parser fagnavn fra record[0]
+        evaluation.set("SUBJECTID", "TMA4100");
         evaluation.set("SUBJECT", subjectPointer);  //Sender videre Subjects-objektet som pointer
-        evaluation.set("NAME", "Øving " + record[0].substring(len - 2, len));   //setter øvingsnavnet
+        evaluation.set("NAME", "Øving 5");
         evaluation.set("rating", record[1]);
         evaluation.set("time", record[2]);
         evaluation.set("lectureAmount", record[3][0]);
@@ -29,10 +35,10 @@ window.submitRecord = function (record) {
         evaluation.set("googleAmount", record[3][2]);
         evaluation.set("solutionsAmount", record[3][3]);
         evaluation.set("comment", record[4]);
-        console.log("Lagret objekt med navn: " + "Øving " + record[0].substring(len - 2, len));
+        //console.log("Lagret objekt med navn: " + "Øving " + record[0].substring(len - 2, len));
         return evaluation.save();
     }).then(function (result) {
-        console.log(".save() sitt callback: " + result);
+        //console.log(".save() sitt callback: " + result);
         //response.succes("CHECK");
         return result;
     }, function (error) {
@@ -64,7 +70,9 @@ function formatData(record){
         total += record[3][i];
     }
     for(var j = 0; j < record[3][j]; j++){
-        record[3][j] = Math.round(record[3][j]/total);
+        record[3][j] = Math.round(record[3][j]*100/total);
     }
+    console.log(record);
+    console.log(record[3]);
     return record;
 }
